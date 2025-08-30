a3adf6357413f9c44685c636967e69c2e0771726
Goldenfoxs <space6941@gmail.com>
2025-08-30 11:55:17 +0300

    Photo add

--- js/main.js
+++ js/main.js
@@ -12,3 +12,15 @@ $(function () {
 
 
 });
+
+$(function () {
+    $('.slider').slick(
+        {
+            dots: true,
+            arrows: false,
+            autoplay: true,
+            autoplayspeed: 1000,
+            Infinity: true,
+        }
+    );
+})
