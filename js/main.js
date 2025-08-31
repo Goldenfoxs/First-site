4ebc910bb628eb22a8c09ee9c05f98fac64590e7
Goldenfoxs <space6941@gmail.com>
2025-08-31 10:15:31 +0300

    update photo

--- js/main.js
+++ js/main.js
@@ -17,10 +17,40 @@ $(function () {
     $('.slider').slick(
         {
             dots: true,
-            arrows: false,
+            arrows: true,
             autoplay: true,
             autoplayspeed: 1000,
             Infinity: true,
+            prevArrow:"<button type='button' class='slick-prev pull-left'><img src='images/arrow-left.svg'></button>",
+            nextArrow:"<button type='button' class='slick-next pull-right'><img src='images/arrow-right.svg'></button>",
+            responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    // centerMode: true,
+                    // centerPadding: '220px',
+                    // slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                        arrows: false,
+                        dots: false,
+                        // centerMode: true,
+                        // centerPadding: '40px',
+                        // slidesToShow: 1
+                }
+            }
+            ]
+            
+
+
+
+
+
+
         }
     );
 })
